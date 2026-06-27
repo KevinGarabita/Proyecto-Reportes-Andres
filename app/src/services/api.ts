cae31f376aa6a36e70657f6228f438689a6114aa
KevinGarabita <kevingarabita0@outlook.com>
2026-06-27 12:14:57 -0600

    correcion de errores

--- app/src/services/api.ts
+++ app/src/services/api.ts
@@ -3,16 +3,15 @@ import { API_URL } from "../config/api";
 export async function apiFetch(endpoint: string, options: RequestInit = {}) {
   const response = await fetch(`${API_URL}${endpoint}`, {
     credentials: "include",
-    ...options,
     headers: {
       "Content-Type": "application/json",
       ...options.headers,
     },
+    ...options,
   });
 
   if (response.status === 401) {
-    window.location.href = "/";
-    return Promise.reject(new Error("No autenticado"));
+    throw new Error("UNAUTHORIZED");
   }
 
   return response;
