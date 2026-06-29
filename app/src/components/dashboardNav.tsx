fe85100957c5f777f4b1e5c184978e207b254c23
KevinGarabita <kevingarabita0@outlook.com>
2026-06-28 23:16:11 -0600

    dashboard

---
+++ app/src/components/dashboardNav.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import type { AuthUser } from "../types/auth";
+
+type DashboardNavProps = { user: AuthUser };
+function DashboardNav({ user }: DashboardNavProps) {
+  const [open, setOpen] = useState(false);
+  return (
+    <nav className="navbar navbar-expand-lg bg-telefield navbar-dark">
+      <div className="container-fluid ">
+        <a className="navbar-brand" href="#">
+          Telefield
+        </a>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          aria-label="Toggle navigation"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className={`collapse navbar-collapse ${open ? "show" : ""}`}>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                Dashboard
+              </a>
+            </li>
+          </ul>
+
+          <div className="d-flex align-items-center text-white">
+            <div className="text-start">
+              <div className="fw-bold">{user.name}</div>
+              <small className="text-white-50">{user.role.toLowerCase()}</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+export default DashboardNav;
