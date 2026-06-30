5f7b0e58e100d86afd974578787a8c1eef96ac3f
KevinGarabita <kevingarabita0@outlook.com>
2026-06-30 17:12:46 -0600

    cambios de diseño

--- app/src/components/pageHeader.tsx
+++ app/src/components/pageHeader.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react";
 
 function PageHeader({ children }: PropsWithChildren) {
   return (
-    <div className="container-fluid bg-Uxmal mb-3 p-4 text-center">
+    <div className="container-fluid bg-Uxmal-nav mb-3 p-4 text-center">
       {children}
     </div>
   );
